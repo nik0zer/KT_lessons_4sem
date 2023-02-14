9fdb74cc974f345e2ea0edd0678cef6515dff023
nik0zer <s21b1.nikozer@gmail.com>
2023-02-14 16:48:17 +0300

    #

--- 2sem/mipt-cs-cpp/oop/main.cpp
+++ 2sem/mipt-cs-cpp/oop/main.cpp
@@ -2,11 +2,12 @@
 
 #include "bank.h"
 
-int main() {
+int main() 
+{
   std::cout << "Hello world" << std::endl;
   
-  Account* acc1 = Bank::createPremium("vasya");
-  Account* acc2 = Bank::create("masha");
+  std::unique_ptr<Account> acc1(Bank::createPremium("vasya"));
+  std::unique_ptr<Account> acc2(Bank::create("masha"));
 
   std::cout<<*acc1<<"\n"<<*acc2<<"\n";
   
@@ -18,15 +19,10 @@ int main() {
   
   std::cout<<*acc1<<std::endl;
   
-  
   acc1->put(m2);
   
   std::cout<<*acc1<<std::endl;
   std::cout<<"put "<<acc1->put(m2)<<std::endl;
   std::cout<<*acc1<<std::endl;
-  std::cout<<"m1 "<<*m1<<std::endl;
-
-  delete acc1;
-  delete acc2;
-  
+  std::cout<<"m1 "<<*m1<<std::endl;  
 }
